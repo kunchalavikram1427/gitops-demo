cb8169b1314aa9197ad3aa25483c6ef3682591a7
vikram <vikram@gmail.com>
2022-01-12 09:27:07 +0530

    modified app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Hello World! FROM MAIN BRANCH!!!"
+    return "Hello from DevOps Made Easy"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
