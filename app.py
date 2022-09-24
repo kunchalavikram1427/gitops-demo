7ff5bf4af68c5af1535e20db07a6b71555cc1857
Pramodh BR <72683141+Pramodh003@users.noreply.github.com>
2022-09-24 12:48:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Hello from DevOps Made Easy for me"
+    return "Hello world"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
